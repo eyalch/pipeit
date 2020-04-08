b5559b3904614385e8175e7c5501de019032a29f
Eyal Cherevatzki <eyal_c@mintapp.co.il>
2020-04-08 12:56:19 +0300

    feat: add Material UI & styled-components

---
+++ src/ThemeProvider.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {
+  MuiThemeProvider,
+  createMuiTheme,
+  StylesProvider,
+} from '@material-ui/core'
+import { createGlobalStyle } from 'styled-components'
+
+const theme = createMuiTheme({
+  shape: { borderRadius: 24 },
+})
+
+const GlobalStyles = createGlobalStyle``
+
+const ThemeProvider: React.FC = ({ children }) => {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <StylesProvider injectFirst>
+        <GlobalStyles />
+
+        {children}
+      </StylesProvider>
+    </MuiThemeProvider>
+  )
+}
+
+export default ThemeProvider
